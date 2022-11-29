8ee92332f8229de92a925774b84a045a943f9173
zuihou <244387066@qq.com>
2022-11-29 17:44:54 +0800

    feat: 新增 表单验证starter 和 web示例

---
+++ examples/easy-validator-web/src/main/java/top/tangyh/lamp/BaseServerApplication.java
@@ -0,0 +1,36 @@
+package top.tangyh.lamp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+/**
+ * 基础服务启动类
+ *
+ * @author zuihou
+ * @date 2021-10-08
+ */
+@SpringBootApplication
+@Configuration
+@Slf4j
+public class BaseServerApplication {
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(BaseServerApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "应用 '{}' 启动成功! 访问连接:\n\t" +
+                        "Swagger文档: \t\thttp://{}:{}/doc.html\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port", "8080")
+        );
+    }
+}
